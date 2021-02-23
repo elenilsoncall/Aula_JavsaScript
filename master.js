0bf167ef7abc3765e5a50c6e5e12f1affca70245
elenilsoncall <elenilson.costa.call@gmail.com>
2021-02-23 00:24:27 -0300

    novo projeto em andamento

---
+++ master.js
@@ -0,0 +1,9 @@
+
+var nome= "Elenilson costa"
+var idade= 29;
+var idade2= 19;
+var frase= "Brasil é o melhor time do mundo"
+//*alert (nome + " tem " + idade + " anos ");
+//*comentarios aqui
+console.log(idade-idade2)
+console.log(frase.toLowerCase());
